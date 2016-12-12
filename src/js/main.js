1d9adb36253c049895c367f7d36b9075e5d289f9
Thomas Wilburn <news@thomaswilburn.net>
2016-12-12 15:31:29 -0800

    Placeholders for the books, initial data binding

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,20 @@
 // require("./lib/ads");
 // var track = require("./lib/tracking");
 
-setTimeout(() => console.log("hello"), 1000);
+var $ = require("./lib/qsa");
+
+var books = $(".book").map(function(b, i) {
+  var data = {};
+  for (var i = 0; i < b.attributes.length; i++) {
+    var attr = b.attributes[i];
+    if (attr.name.match(/^data-/)) {
+      data[attr.name.replace("data-", "")] = attr.value;
+    }
+  }
+  data.genre = data.genre.split(/;\s*/);
+  data.element = b;
+  b.setAttribute("data-index", i);
+  return data;
+});
+
+console.log(books);
